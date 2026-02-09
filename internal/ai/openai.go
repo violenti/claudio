b0b44beafe6eb8cb584e190439c80078514aefa5
David Fernandez <rodavidfernandez@gmail.com>
2026-02-09 14:45:41 -0300

    feat: add support of openia

--- internal/ai/openai.go
+++ internal/ai/openai.go
@@ -1,7 +1,16 @@
 package ai
 
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/openai/openai-go/v3"
+)
+
 type OpenAI struct {
 	Token string
+	Model string
 }
 
 func (o OpenAI) Name() string {
@@ -9,5 +18,33 @@ func (o OpenAI) Name() string {
 }
 
 func (o OpenAI) Question(p string) (string, error) {
-	return "Question of ChatGPT...", nil
+	Token := o.Token
+	if Token == "" {
+		Token = os.Getenv("OPENAI_API_KEY")
+	}
+	if Token == "" {
+		return "", fmt.Errorf("OPENAI_API_KEY not configured")
+	}
+
+	client := openai.NewClient()
+	ctx := context.Background()
+
+	params := openai.ChatCompletionNewParams{
+		Messages: []openai.ChatCompletionMessageParamUnion{
+			openai.UserMessage(p),
+		},
+		Seed:  openai.Int(0),
+		Model: openai.ChatModelGPT5_2,
+	}
+
+	completion, err := client.Chat.Completions.New(ctx, params)
+
+	if err != nil {
+		return "", fmt.Errorf("error of API (OpenIA): %w", err)
+	}
+	if len(completion.Choices) > 0 {
+		return completion.Choices[0].Message.Content, nil
+	}
+
+	return "I received no response from the AI", nil
 }
